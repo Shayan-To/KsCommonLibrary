5d60f1ffb8fac972ad4ecdc7e58053c1136ee1fe
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-02-08 12:22:59 +0330

    Added EventBasedTask, an awaitable.
    A sample implementation of an awaitable. Just sample.

---
+++ Common/EventBasedTask.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Common
+
+    <Obsolete("Use " + NameOf(TaskCompletionSource(Of Void)) + " instead.", True)>
+    Public Class EventBasedTask
+        Implements INotifyCompletion
+
+        Public Function GetAwaiter() As EventBasedTask
+            Return Me
+        End Function
+
+        Public Sub GetResult()
+            Verify.True(Me.IsCompleted)
+        End Sub
+
+        Public Sub SetComplete()
+            If Not Me._IsCompleted Then
+                Me._IsCompleted = True
+                Me.CompletedAction?.Invoke()
+            End If
+        End Sub
+
+        Public Sub OnCompleted(continuation As Action) Implements INotifyCompletion.OnCompleted
+            Verify.True(Me.CompletedAction Is Nothing)
+            Me.CompletedAction = continuation
+        End Sub
+
+#Region "IsCompleted Property"
+        Private _IsCompleted As Boolean
+
+        Public ReadOnly Property IsCompleted As Boolean
+            Get
+                Return Me._IsCompleted
+            End Get
+        End Property
+#End Region
+
+        Private CompletedAction As Action
+
+    End Class
+
+End Namespace
