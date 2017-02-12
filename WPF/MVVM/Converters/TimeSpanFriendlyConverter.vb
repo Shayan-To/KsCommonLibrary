ae8180f538008379e774bc960cf20529b195d488
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-02-12 18:40:07 +0330

    Added MVVM.TimeSpanFriendlyConverter.

---
+++ WPF/MVVM/Converters/TimeSpanFriendlyConverter.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Globalization
+Imports System.Windows.Data
+
+Namespace Common.MVVM.Converters
+
+    Public Class TimeSpanFriendlyConverter
+        Implements IValueConverter
+
+        Public Function Convert(ByVal Value As Object, ByVal TargetType As Type, ByVal Parameter As Object, ByVal Culture As CultureInfo) As Object Implements IValueConverter.Convert
+            Return Utilities.GetFriendlyRepresentation(CType(Value, TimeSpan), TimeSpan.FromMinutes(CType(Parameter, Double)))
+        End Function
+
+        Public Function ConvertBack(ByVal Value As Object, ByVal TargetType As Type, ByVal Parameter As Object, ByVal Culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+            ' ToDo Implement the reverse conversion.
+            Throw New NotSupportedException()
+        End Function
+
+    End Class
+
+End Namespace
