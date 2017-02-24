84924ad129eb7934291f7ecbca719f72bfcd3770
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-02-24 11:41:39 +0330

    MVVM.BindableBase: Removed the compatibility (Old VB) preprocessor lines.

--- WPF/MVVM/BindableBase.vb
+++ WPF/MVVM/BindableBase.vb
@@ -7,11 +7,7 @@ Namespace Common.MVVM
 
         Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
 
-#If VBC_VER >= 11.0 Then
         Protected Function SetProperty(Of T)(ByRef Source As T, ByVal Value As T, <Runtime.CompilerServices.CallerMemberName()> Optional ByVal PropertyName As String = Nothing) As Boolean
-#Else
-        Protected Function SetProperty(Of T)(ByRef Source As T, ByVal Value As T, ByVal PropertyName As String) As Boolean
-#End If
             If Not Object.Equals(Source, Value) Then
                 Source = Value
                 Me.NotifyPropertyChanged(PropertyName)
@@ -25,11 +21,7 @@ Namespace Common.MVVM
             RaiseEvent PropertyChanged(Me, E)
         End Sub
 
-#If VBC_VER >= 11.0 Then
         Protected Sub NotifyPropertyChanged(<Runtime.CompilerServices.CallerMemberName()> Optional ByVal PropertyName As String = Nothing)
-#Else
-        Protected Sub NotifyPropertyChanged(ByVal PropertyName As String)
-#End If
             Me.OnPropertyChanged(New PropertyChangedEventArgs(PropertyName))
         End Sub
 
