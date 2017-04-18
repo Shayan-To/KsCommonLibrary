9e667304d3e4aa51e367f732230d1463c953c309
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-04-18 18:25:41 +0430

    MergedList: Set BaseReadOnlyList as the base class. +Few fixes.

--- Collections/MergedList.vb
+++ Collections/MergedList.vb
@@ -1,32 +1,26 @@
 ﻿Namespace Common
 
     Public Class MergedList(Of T)
-        Implements IList(Of T)
+        Inherits BaseReadOnlyList(Of T)
 
-        Public Sub New(ByVal Lists As IEnumerable(Of IList(Of T)))
+        Public Sub New(ByVal Lists As IEnumerable(Of IReadOnlyList(Of T)))
             Me.Lists = Lists.ToArray()
         End Sub
 
-        Public Sub New(ParamArray ByVal Lists As IList(Of T)())
-            Me.New(DirectCast(Lists, IEnumerable(Of IList(Of T))))
+        Public Sub New(ParamArray ByVal Lists As IReadOnlyList(Of T)())
+            Me.New(DirectCast(Lists, IEnumerable(Of IReadOnlyList(Of T))))
         End Sub
 
-        Public ReadOnly Property Count As Integer Implements ICollection(Of T).Count
+        Public Overrides ReadOnly Property Count As Integer
             Get
                 Return Me.Lists.Sum(Function(L) L.Count)
             End Get
         End Property
 
-        Public ReadOnly Property IsReadOnly As Boolean Implements ICollection(Of T).IsReadOnly
-            Get
-                Return True
-            End Get
-        End Property
-
-        Default Public Property Item(Index As Integer) As T Implements IList(Of T).Item
+        Default Public Overrides ReadOnly Property Item(Index As Integer) As T
             Get
                 For Each L In Me.Lists
-                    Dim Count As Integer = L.Count
+                    Dim Count = L.Count
                     If Index < Count Then
                         Return L.Item(Index)
                     End If
@@ -34,47 +28,16 @@
                 Next
                 Throw New ArgumentOutOfRangeException(NameOf(Index))
             End Get
-            Set(Value As T)
-                Throw New NotSupportedException()
-            End Set
         End Property
 
-        Public Sub Add(item As T) Implements ICollection(Of T).Add
-            Throw New NotSupportedException()
-        End Sub
-
-        Public Sub Clear() Implements ICollection(Of T).Clear
-            Throw New NotSupportedException()
-        End Sub
-
-        Public Sub CopyTo(array() As T, arrayIndex As Integer) Implements ICollection(Of T).CopyTo
+        Public Overrides Sub CopyTo(array() As T, arrayIndex As Integer)
             For Each L In Me.Lists
                 L.CopyTo(array, arrayIndex)
                 arrayIndex += L.Count
             Next
         End Sub
 
-        Public Sub Insert(index As Integer, item As T) Implements IList(Of T).Insert
-            Throw New NotSupportedException()
-        End Sub
-
-        Public Sub RemoveAt(index As Integer) Implements IList(Of T).RemoveAt
-            Throw New NotSupportedException()
-        End Sub
-
-        Public Function Contains(item As T) As Boolean Implements ICollection(Of T).Contains
-            Throw New NotSupportedException()
-        End Function
-
-        Public Iterator Function GetEnumerator() As IEnumerator(Of T) Implements IEnumerable(Of T).GetEnumerator
-            For Each L In Me.Lists
-                For Each I In L
-                    Yield I
-                Next
-            Next
-        End Function
-
-        Public Function IndexOf(item As T) As Integer Implements IList(Of T).IndexOf
+        Public Overrides Function IndexOf(item As T) As Integer
             Dim I = 0
             For Each L In Me.Lists
                 For Each It In L
@@ -87,15 +50,15 @@
             Return -1
         End Function
 
-        Public Function Remove(item As T) As Boolean Implements ICollection(Of T).Remove
-            Throw New NotSupportedException()
+        Public Overrides Iterator Function GetEnumerator() As IEnumerator(Of T)
+            For Each L In Me.Lists
+                For Each I In L
+                    Yield I
+                Next
+            Next
         End Function
 
-        Private Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
-            Return Me.GetEnumerator()
-        End Function
-
-        Private ReadOnly Lists As IList(Of T)()
+        Private ReadOnly Lists As IReadOnlyList(Of T)()
 
     End Class
 
