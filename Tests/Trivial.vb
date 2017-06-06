baf41297fd2f28f6b78d6cb543df5cf9cdabd87d
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-06-06 17:58:26 +0430

    Add unit tests project.

---
+++ Tests/Trivial.vb
@@ -0,0 +1,13 @@
+﻿Public Class Trivial
+
+    <Fact()>
+    Public Sub IsTrue()
+        Assert.False(False)
+    End Sub
+
+    <Fact()>
+    Public Sub IsNotFalse()
+        Assert.False(False)
+    End Sub
+
+End Class
