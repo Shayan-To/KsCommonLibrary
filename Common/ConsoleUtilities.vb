779b4ae51073a68b4511af35175f11fc6cdd1f8d
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-07-20 11:09:58 +0430

    ConsoleUtilities: Add WriteColored and ReadYesNo.

--- Common/ConsoleUtilities.vb
+++ Common/ConsoleUtilities.vb
@@ -12,11 +12,32 @@
             Console.Clear()
         End Sub
 
+        Public Shared Sub WriteColored(ByVal Value As String, ByVal Color As ConsoleColor)
+            Dim PrevColor = Console.BackgroundColor
+            Console.BackgroundColor = Color
+            Console.Write(Value)
+            Console.BackgroundColor = PrevColor
+        End Sub
+
+        Public Shared Function ReadYesNo(ByVal Prompt As String) As Boolean
+            WriteColored(Prompt, ConsoleColor.Green)
+
+            Dim K As ConsoleKey
+            Do
+                K = Console.ReadKey(True).Key
+            Loop Until K = ConsoleKey.Y Or K = ConsoleKey.N
+
+            Dim Res = K = ConsoleKey.Y
+
+            Console.WriteLine(If(Res, " Y", " N"))
+
+            Return Res
+        End Function
+
         Public Shared Sub WriteExceptionData(ByVal Ex As Exception)
             Do
-                Console.BackgroundColor = ConsoleColor.Yellow
-                Console.WriteLine("Exception: --------------------------------------------------")
-                Console.BackgroundColor = ConsoleColor.White
+                WriteColored("Exception: --------------------------------------------------", ConsoleColor.Yellow)
+                Console.WriteLine()
                 Console.WriteLine("Type: " & Ex.GetType().FullName)
                 Console.WriteLine("Message: " & Ex.Message)
                 Console.WriteLine("StackTrace: " & Ex.StackTrace)
@@ -33,9 +54,7 @@
 
         Public Shared Sub Pause()
             If Environment.UserInteractive Then
-                Console.BackgroundColor = ConsoleColor.Green
-                Console.Write("Press any key to continue...")
-                Console.BackgroundColor = ConsoleColor.White
+                WriteColored("Press any key to continue...", ConsoleColor.Green)
                 Console.ReadKey(True)
                 Console.WriteLine()
             End If
