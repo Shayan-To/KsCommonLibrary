50c4fa5c3107d38d6aca207664da5d60e3e73c3b
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-08-16 15:42:06 +0430

    ConsoleTests: Move test files from other projects to here.

---
+++ ConsoleTests/WordReaderTest.vb
@@ -0,0 +1,13 @@
+﻿Public Class WordReaderTest
+
+    <InteractiveRunnable(True)>
+    Public Shared Sub Start()
+        Dim Reader = New WordReader(Console.In)
+
+        Do
+            Dim W = Reader.ReadWord()
+            Console.WriteLine(W)
+        Loop
+    End Sub
+
+End Class
