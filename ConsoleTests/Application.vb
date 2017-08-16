e4dc6f8b255cce833fb6b63cf2f647da8c10ec0f
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-08-16 10:54:37 +0430

    Add ConsoleTests project.

---
+++ ConsoleTests/Application.vb
@@ -0,0 +1,13 @@
+﻿Public MustInherit Class Application
+
+    Private Sub New()
+        Throw New NotSupportedException()
+    End Sub
+
+    Public Shared Sub Main()
+        ConsoleUtilities.Initialize()
+        InteractiveRunnableAttribute.RunTestMethods()
+        ConsoleUtilities.Pause()
+    End Sub
+
+End Class
