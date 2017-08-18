6d250958675d5f40c3b230a1b45d8c66d1783715
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-08-18 16:19:44 +0430

    Add QuickSorter + unit tests.
    Thanks to @S.Khansari!

---
+++ Tests/Common/Sorters_Tests.vb
@@ -0,0 +1,18 @@
+﻿Namespace Common
+
+    Public Class Sorters_Tests
+
+        <[Property]()>
+        Public Sub QuickSort(ByVal List As Integer())
+            Dim Clone = New Integer(List.Length - 1) {}
+            List.CopyTo(Clone, 0)
+
+            QuickSorter(Of Integer).Instance.Sort(List)
+            Array.Sort(Clone)
+
+            Assert.Equal(List, Clone)
+        End Sub
+
+    End Class
+
+End Namespace
