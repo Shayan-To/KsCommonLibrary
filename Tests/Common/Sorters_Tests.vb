24091760bf26093b7b0ca8693c1f013f09270b2d
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-08-18 16:20:18 +0430

    Tests.Sorters: Add tests for Merge and MergeSort.

--- Tests/Common/Sorters_Tests.vb
+++ Tests/Common/Sorters_Tests.vb
@@ -3,6 +3,37 @@
     Public Class Sorters_Tests
 
         <[Property]()>
+        Public Sub Merge(ByVal List1 As Integer(), ByVal List2 As Integer())
+            Dim Clone = New Integer(List1.Length + List2.Length - 1) {}
+            List1.CopyTo(Clone, 0)
+            List2.CopyTo(Clone, List1.Length)
+
+            Array.Sort(List1)
+            Array.Sort(List2)
+
+            Dim List = New Integer(List1.Length + List2.Length - 1) {}
+            MergeSorter(Of Integer).Merge(List, 0,
+                                          List1, 0, List1.Length,
+                                          List2, 0, List2.Length,
+                                          Comparer(Of Integer).Default)
+
+            Array.Sort(Clone)
+
+            Assert.Equal(List, Clone)
+        End Sub
+
+        <[Property]()>
+        Public Sub MergeSort(ByVal List As Integer())
+            Dim Clone = New Integer(List.Length - 1) {}
+            List.CopyTo(Clone, 0)
+
+            MergeSorter(Of Integer).Instance.Sort(List)
+            Array.Sort(Clone)
+
+            Assert.Equal(List, Clone)
+        End Sub
+
+        <[Property]()>
         Public Sub QuickSort(ByVal List As Integer())
             Dim Clone = New Integer(List.Length - 1) {}
             List.CopyTo(Clone, 0)
