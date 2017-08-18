0bd87cc6ec42e618d8ddd3193ebd122d7dd25976
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-08-18 16:16:27 +0430

    DefaultCacher: Change to create a separate instance for each thread.

--- Common/DefaultCacher.vb
+++ Common/DefaultCacher.vb
@@ -2,11 +2,11 @@
 
     Public Class DefaultCacher(Of T As New)
 
-        Private Shared ReadOnly _Value As T = New T()
+        Private Shared ReadOnly _Value As Threading.ThreadLocal(Of T) = New Threading.ThreadLocal(Of T)(Function() New T())
 
         Public Shared ReadOnly Property Value As T
             Get
-                Return _Value
+                Return _Value.Value
             End Get
         End Property
 
