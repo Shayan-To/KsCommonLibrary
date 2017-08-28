c98d37d76a3a7dc811e45168406f59b3fe8c3b67
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-08-28 15:56:57 +0430

    InsertionSorter: Rename to InsertionSortList, and inherit from BaseList.

---
+++ Collections/InsertionSortList.vb
@@ -0,0 +1,68 @@
+﻿Namespace Common
+
+    Public Class InsertionSortList(Of T)
+        Inherits BaseList(Of T)
+
+        Public Sub New()
+            Me.New(Generic.Comparer(Of T).Default)
+        End Sub
+
+        Public Sub New(ByVal Comparer As IComparer(Of T))
+            Me.Comparer = Comparer
+        End Sub
+
+        Public Overrides Sub Insert(index As Integer, item As T)
+            Throw New NotSupportedException()
+        End Sub
+
+        Public Overrides Sub Add(item As T)
+            Throw New NotImplementedException()
+            Dim I = Me.List.First
+            Do
+                If Me.Comparer.Compare(I.Value, item) > 0 Then
+
+                End If
+            Loop
+        End Sub
+
+        Public Overrides Function IndexOf(item As T) As Integer
+            Throw New NotImplementedException()
+        End Function
+
+        Public Overrides Sub RemoveAt(index As Integer)
+            Throw New NotImplementedException()
+        End Sub
+
+        Public Overrides Sub Clear()
+            Throw New NotImplementedException()
+        End Sub
+
+        Protected Overrides Function IEnumerable_1_GetEnumerator() As IEnumerator(Of T)
+            Return Me.GetEnumerator()
+        End Function
+
+        Public Function GetEnumerator() As IEnumerator(Of T)
+            Throw New NotImplementedException()
+        End Function
+
+        Default Public Overrides Property Item(index As Integer) As T
+            Get
+                Throw New NotImplementedException()
+            End Get
+            Set(value As T)
+                Throw New NotImplementedException()
+            End Set
+        End Property
+
+        Public Overrides ReadOnly Property Count As Integer
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
+
+        Private ReadOnly Comparer As IComparer(Of T)
+        Private ReadOnly List As LinkedList(Of T) = New LinkedList(Of T)()
+
+    End Class
+
+End Namespace
