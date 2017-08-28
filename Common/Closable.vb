bfd13245beb25d2b91d6cb9d8bbcc34f98b9909d
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-08-28 15:51:50 +0430

    Add Closable, a disposable structure calling a custom delegate at disposal.

---
+++ Common/Closable.vb
@@ -0,0 +1,22 @@
+﻿Namespace Common
+
+    Public Class Closable
+        Implements IDisposable
+
+        Public Sub New(ByVal CloseOperation As Action)
+            Me.CloseOperation = CloseOperation
+        End Sub
+
+        Public Sub Close()
+            Me.CloseOperation.Invoke()
+        End Sub
+
+        Private Sub Dispose() Implements IDisposable.Dispose
+            Me.Close()
+        End Sub
+
+        Private ReadOnly CloseOperation As Action
+
+    End Class
+
+End Namespace
