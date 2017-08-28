ab00105422c3eceec596913c02f06a30f1e6ad0a
Shayan Toqraee <Shayan.To@Hotmail.com>
2017-08-28 15:55:45 +0430

    Add Dispatcher (and InvocationException).

---
+++ Common/InvocationException.vb
@@ -0,0 +1,20 @@
+﻿Namespace Common
+
+    Public Class InvocationException
+        Inherits Exception
+
+        Public Sub New()
+            MyBase.New()
+        End Sub
+
+        Public Sub New(message As String)
+            MyBase.New(message)
+        End Sub
+
+        Public Sub New(message As String, innerException As Exception)
+            MyBase.New(message, innerException)
+        End Sub
+
+    End Class
+
+End Namespace
