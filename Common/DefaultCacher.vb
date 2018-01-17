f1ba7f6f725b765ae43dda271b8769ad7d3ba8ad
Shayan Toqraee <Shayan.To@Hotmail.com>
2018-01-17 16:38:12 +0330

    DefaultCacher: Add SetValue method. +Code convention fixes.

--- Common/DefaultCacher.vb
+++ Common/DefaultCacher.vb
@@ -1,6 +1,14 @@
 ﻿Namespace Common
 
-    Public Class DefaultCacher(Of T As New)
+    Public MustInherit Class DefaultCacher(Of T As New)
+
+        Private Sub New()
+            Throw New NotSupportedException()
+        End Sub
+
+        Public Shared Sub SetValue(ByVal Value As T)
+            _Value.Value = Value
+        End Sub
 
         Private Shared ReadOnly _Value As Threading.ThreadLocal(Of T) = New Threading.ThreadLocal(Of T)(Function() New T())
 
