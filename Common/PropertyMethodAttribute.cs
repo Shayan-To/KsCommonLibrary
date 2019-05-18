d03a8368eff3440ce0e00597b776e2c0ca4b5fb2
Shayan Toqraee <Shayan.To@Hotmail.com>
2019-05-18 23:32:29 +0430

    Add PropertyMethodAttribute.

---
+++ Common/PropertyMethodAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ks.Common
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class PropertyMethodAttribute : Attribute
+    {
+    }
+}
