83506b81da4b9e20cdcee0695d13d7ec330e9b8e
Shayan Toqraee <Shayan.To@Hotmail.com>
2020-12-01 17:26:47 +0330

    DefaultCacher: Add support for custom factory functions.

--- Common/DefaultCacher.cs
+++ Common/DefaultCacher.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace Ks.Common
 {
-    public static class DefaultCacher<T> where T : new()
+    public static class DefaultCacher<T>
     {
 
         public static void SetValue(T Value)
@@ -10,8 +12,39 @@ namespace Ks.Common
             _Value.Value = Value;
         }
 
-        private static readonly System.Threading.ThreadLocal<T> _Value = new System.Threading.ThreadLocal<T>(() => new T());
+        private static readonly System.Threading.ThreadLocal<T> _Value = new System.Threading.ThreadLocal<T>(Initialize);
+
+        private static T Initialize()
+        {
+            return DefaultCacher.CreateInstance<T>();
+        }
 
         public static T Value => _Value.Value;
+    }
+
+    public static class DefaultCacher
+    {
+        private static readonly Dictionary<Type, object> Initializers = new Dictionary<Type, object>();
+
+        public static void AddTypeInitializer<T>(Func<T> initializer)
+        {
+            Initializers[typeof(T)] = initializer;
+        }
+
+        public static T CreateInstance<T>()
+        {
+            if (Initializers.TryGetValue(typeof(T), out var initializer))
+            {
+                return ((Func<T>) initializer).Invoke();
+            }
+
+            return (T) typeof(T).CreateInstance();
+        }
+
+        static DefaultCacher()
+        {
+            AddTypeInitializer(() => RandomNumberGenerator.Create());
+        }
+
     }
 }
