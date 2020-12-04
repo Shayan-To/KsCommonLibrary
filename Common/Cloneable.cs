3ee2215f06d106f8ccd1478fd006db1002822095
Shayan Toqraee <Shayan.To@Hotmail.com>
2020-12-04 15:56:08 +0330

    Add Clonable.

---
+++ Common/Cloneable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ks.Common
+{
+    public abstract class Cloneable<T> : ICloneable where T : Cloneable<T>
+    {
+
+        public T Clone()
+        {
+            var c = this.CloneOverride();
+            Verify.True(this.GetType() == c.GetType(), "Invalid cloneable implementation.");
+            return c;
+        }
+
+        object ICloneable.Clone()
+        {
+            return this.Clone();
+        }
+
+        protected abstract T CloneOverride();
+    }
+}
