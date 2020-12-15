3f9019c8f010c09058864b1669d89695dcf4a2c5
Shayan Toqraee <Shayan.To@Hotmail.com>
2020-12-15 22:13:33 +0330

    Add DefaultValue.

---
+++ Common/DefaultValue.cs
@@ -0,0 +1,28 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ks.Common
+{
+    public static class DefaultValue
+    {
+
+        public static T? Get<T>(string? tag = null)
+        {
+            for (var i = 0; i < Generators.Count; i++)
+            {
+                var g = Generators[i];
+                var gg = g.Invoke(typeof(T), tag);
+                if (gg.Item1)
+                {
+                    return (T?) gg.Item2;
+                }
+            }
+            return default;
+        }
+
+        public static List<Func<Type, string?, (bool, object?)>> Generators { get; } = new();
+    }
+}
